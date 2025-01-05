dda5384b460107e6c74a59f298e919cb6fbee1ab
YuriiBalandyuk <juraitclash@gmail.com>
2025-01-05 16:07:17 +0200

    Fix errors in the code

--- main.py
+++ main.py
@@ -29,26 +29,34 @@ from random import randrange
 * реалізувати функції, які наведені у редакторі.
 """
 
-def display_view_board():
-    print("+---------" * 3 + "+" + "\n" + "|         " * 3 + "|" + "\n" + "|   ", board[0][0], " " + "  |   ",
-          board[0][1], " " + "  |   ", board[0][2], "   |" + "\n" + "|         " * 3 + "|" \
-          + "\n" + "+---------" * 3 + "+" + "\n" + "|         " * 3 + "|" + "\n" + "|   ", board[1][0], " " + "  |   ",
-          board[1][1], " " + "  |   ", board[1][2], "   |" + "\n" + "|         " * 3 + "|" \
-          + "\n" + "+---------" * 3 + "+" + "\n" + "|         " * 3 + "|" + "\n" + "|   ", board[2][0], " " + "  |   ",
-          board[2][1], " " + "  |   ", board[2][2], "   |" + "\n" + "|         " * 3 + "|" \
+# Функція, що виводить поточний статус дошки.
+
+#date: 05.01.2025
+def display_view_board(board_):
+    print("+---------" * 3 + "+" + "\n" + "|         " * 3 + "|" + "\n" + "|   ", board_[0][0], " " + "  |   ",
+          board_[0][1], " " + "  |   ", board_[0][2], "   |" + "\n" + "|         " * 3 + "|" \
+          + "\n" + "+---------" * 3 + "+" + "\n" + "|         " * 3 + "|" + "\n" + "|   ", board_[1][0], " " + "  |   ",
+          board_[1][1], " " + "  |   ", board_[1][2], "   |" + "\n" + "|         " * 3 + "|" \
+          + "\n" + "+---------" * 3 + "+" + "\n" + "|         " * 3 + "|" + "\n" + "|   ", board_[2][0], " " + "  |   ",
+          board_[2][1], " " + "  |   ", board_[2][2], "   |" + "\n" + "|         " * 3 + "|" \
           + "\n" + "+---------" * 3 + "+"
           )
 
-def display_board(board):
-    board[1][1] = "X"
-    display_view_board()
-    return board
-    # Функція приймає один параметр, що містить поточний статус дошки
-    # і виводить його на консоль.
+# Функція приймає один параметр, що містить поточний статус дошки
+# і виводить його на консоль.
+
+#date: 05.01.2025
+def display_board(board_):
+    board_[1][1] = "X"
+    display_view_board(board_)
+    return board_
 
 
+# Функція приймає поточний статус дошки, запитує користувача про його хід,
+# перевіряє введення та оновлює дошку відповідно до рішення користувача.
 
-def enter_move(board):
+#date: 05.01.2025
+def enter_move(board_):
     print("Тепер ваша черга ходити. Виберіть число від 1 до 9 (врахуйте зайняті клітинки).")
 
     while True:
@@ -58,105 +66,92 @@ def enter_move(board):
             print("Це не є числом!")
             continue
 
-        for i in range(len(board)):
-            for j in range(len(board[i])):
-                if number == board[i][j]:
-                    board[i][j] = "0"
-                    return board
+        for row_ in range(len(board_)):
+            for column in range(len(board_[row_])):
+                if number == board_[row_][column]:
+                    board_[row_][column] = "0"
+                    return board_
         else:
             print("Цей квадрат вже зайнятий!")
 
-    # Функція приймає поточний статус дошки, запитує користувача про його хід,
-    # перевіряє введення та оновлює дошку відповідно до рішення користувача.
+# Функція аналізує стан дошки, щоб перевірити, чи
+# э в грі переможець
 
+#date: 05.01.2025
+def winner_for(board_):
 
-
-# def make_list_of_free_fields(board):
-#     # Функція перевіряє дошку та створює список усіх вільних квадратів;
-#     # список складається з кортежів, так що кожен кортеж є парою номерів рядка і стовпчика.
-#
-#
-
-def winner_for(board):
-
-    count_0 = 0
-    count_X = 0
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            if board[i][j] == "0":
+    for row_ in range(len(board_)):
+        count_0 = 0
+        count_x = 0
+        for column in range(len(board_[row_])):
+            if board_[row_][column] == "0":
                 count_0 += 1
                 if count_0 == 3:
                     return "Гравець виграв"
-            elif board[i][j] == "X":
-                count_X += 1
-                if count_X == 3:
+            elif board_[row_][column] == "X":
+                count_x += 1
+                if count_x == 3:
                     return "Комп'ютер виграв"
-        count_0 = 0
-        count_X = 0
 
-    for count_column in range(len(board[0])):  # Ітерація по колонках
-        for i in range(len(board)):  # Ітерація по рядках
-            if board[i][count_column] == "0":
+    for count_column in range(len(board_[0])):
+        count_0 = 0
+        count_x = 0
+        for row_ in range(len(board_)):
+            if board_[row_][count_column] == "0":
                 count_0 += 1
                 if count_0 == 3:
                     return "Гравець виграв"
-            elif board[i][count_column] == "X":
-                count_X += 1
-                if count_X == 3:
+            elif board_[row_][count_column] == "X":
+                count_x += 1
+                if count_x == 3:
                     return "Комп'ютер виграв"
-        count_0 = 0
-        count_X = 0
 
-    for i in range(len(board)):
-        if board[i][i] == "0":
+    for row_ in range(len(board_)):
+        count_0 = 0
+        count_x = 0
+        if board_[row_][row_] == "0":
             count_0 += 1
             if count_0 == 3:
                  return "Гравець виграв"
-        elif board[i][i] == "X":
-            count_X += 1
-            if count_X == 3:
+        elif board_[row_][row_] == "X":
+            count_x += 1
+            if count_x == 3:
                 return "Комп'ютер виграв"
-        count_0 = 0
-        count_X = 0
 
     count_column = 2
-    for i in range(len(board)):
-        if board[i][count_column] == "0":
+    for row_ in range(len(board_)):
+        count_0 = 0
+        count_x = 0
+        if board_[row_][count_column] == "0":
             count_0 += 1
             if count_0 == 3:
                 return "Гравець виграв"
             count_column -= 1
-        elif board[i][count_column] == "X":
-            count_X += 1
-            if count_X == 3:
+        elif board_[row_][count_column] == "X":
+            count_x += 1
+            if count_x == 3:
                 return "Комп'ютер виграв"
             count_column -= 1
-        elif board[i][count_column] != "0" or board[i][count_column] != "X":
+        elif board_[row_][count_column] != "0" or board_[row_][count_column] != "X":
             count_column -= 1
-        count_0 = 0
-        count_X = 0
 
-    # Функція аналізує стан дошки, щоб перевірити, чи
-    # э в грі переможець
+# Функція малює хід комп'ютера та оновлює дошку.
 
-
-
-def draw_move(board):
+#date: 05.01.2025
+def draw_move(board_):
 
     while True:
         number = randrange(1,10)
 
-        for i in range(len(board)):
-            for j in range(len(board[i])):
-                if number == board[i][j]:
-                    board[i][j] = "X"
-                    return board
-
-    # Функція малює хід комп'ютера та оновлює дошку.
+        for row_ in range(len(board)):
+            for column in range(len(board[row_])):
+                if number == board_[row_][column]:
+                    board_[row_][column] = "X"
+                    return board_
 
 while True:
-
-    board = [] # Основний список для масиву 3x3
+    # Основний список для масиву 3x3
+    board = []
 
     for i in range(3):
         start_ = i * 3 + 1
@@ -166,7 +161,6 @@ while True:
             row.append(j)
         board.append(row)
 
-    choose_status = " "
     while True:
         print("\nДавайте зіграємо в гру. Для початку гри введіть 'Yes'. Для виходу з програми введіть 'No'.")
         choose_status = input("Введіть вашу відповідь: ")
@@ -177,7 +171,7 @@ while True:
             break
 
     print("\nПочатковий вигляд сітки для гри\n")
-    display_view_board()
+    display_view_board(board)
     print("\nПерший хід за комп'ютером\n")
 
     modify_border = []
@@ -187,7 +181,7 @@ while True:
         if status_game == "Гравець виграв":
             print("Гравець виграв")
             break
-        elif status_game == "Комп'ютер виграв":
+        if status_game == "Комп'ютер виграв":
             print("Комп'ютер виграв")
             break
         modify_border = enter_move(modify_border)
@@ -195,16 +189,11 @@ while True:
         if status_game == "Гравець виграв":
             print("Гравець виграв")
             break
-        elif status_game == "Комп'ютер виграв":
-            print("Комп'ютер виграв")
-            break
         modify_border = draw_move(modify_border)
-        if status_game == "Гравець виграв":
-            print("Гравець виграв")
-            break
-        elif status_game == "Комп'ютер виграв":
+        if status_game == "Комп'ютер виграв":
             print("Комп'ютер виграв")
             break
+
 
 
 
